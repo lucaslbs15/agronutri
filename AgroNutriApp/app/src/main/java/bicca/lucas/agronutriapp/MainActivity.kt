fe77077c443a75a55ba3a348a95bd07cf5cf7f15
Lucas Bicca <lucaslbs15@gmail.com>
2018-09-19 22:32:34 -0300

    Supporting for data binding

--- AgroNutriApp/app/src/main/java/bicca/lucas/agronutriapp/MainActivity.kt
+++ AgroNutriApp/app/src/main/java/bicca/lucas/agronutriapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package bicca.lucas.agronutriapp
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import bicca.lucas.agronutriapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
